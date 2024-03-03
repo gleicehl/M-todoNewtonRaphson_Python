702153a4ed48a8fa2771b4638fe7ef2761da845c
Gleice Heloise <111431766+gleicehl@users.noreply.github.com>
2024-03-03 19:08:31 -0300

    Update metodo.py

--- metodo.py
+++ metodo.py
@@ -25,21 +25,3 @@ def newton_raphson(f, derivF, x0, t, max=100):
     valX.append(x1)
 
   raise RuntimeError("O método ultrapassou {} iterações.".format(max))
-
-# Exemplo de uso:
-import math
-
-# Definindo a função e sua derivada
-def f(x0):
-    return x0**3 - 2*x0 - 5
-
-def derivF(x0):
-    return 3*x0**2 - 2
-
-# Aproximação inicial e parâmetros
-x0 = 2.5
-t = 1e-6
-max=100
-# Chamando a função para calcular a raiz
-raiz = newton_raphson(f, derivF, x0, t, max)
-print("A raiz é:", raiz)
